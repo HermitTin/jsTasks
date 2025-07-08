7131ee34102569aac086ca9101a1457df9e3c2ff
HermitTin <alexprykhodko96@gmail.com>
2025-07-09 00:32:13 +0300

    Delete tasks/1 directory

--- tasks/1/1.5.4.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-// Даны два целых числа. Найдите остаток от деления первого числа на второе.
-
-let numberFirst = +prompt("Enter first number", 1);
-let numberSecond = +prompt("Enter second number", 2);
-
-console.log(`THe remeiner of dividing ${numberFirst} by ${numberSecond} is ${numberFirst % numberSecond}`);
--- tasks/1/1.9.3.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-// Дан массив с числами. Увеличьте каждое число из массива на 10 процентов.
-
-let arr = [1, 2, 3, 4, 5];
-
-arr.forEach((value, index, array) => array[index] = (value * 1.1).toFixed(1));
-
-console.log(arr);

16b725837b1b0460f01561917c6dd03f5e975118
HermitTin <alexprykhodko96@gmail.com>
2025-07-14 19:11:36 +0300

    Add files via upload

--- tasks/2/2.3.2.js
+++ tasks/2/2.3.2.js
@@ -4,10 +4,10 @@
 
 let str = "0wq wdq0 qwd 0 qw0 d0";
 let counter = 0;
-let position = 0;
+let position = -1;
 
 do {
-    position = str.indexOf("0", counter + position);
+    position = str.indexOf("0", position + 1);
     counter++;
 } while (counter < 3)
 
